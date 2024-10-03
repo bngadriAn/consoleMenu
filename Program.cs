9497bbc0cb671c20e1b7b971cb7a918771baf6fb
kazai.daniel.gabor <kazai.daniel.gabor@BOT218-002>
2024-10-03 12:27:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Principal;
+
+namespace _1003_órai_console_léptetés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write('┌');
+            for (int i = 1; i <= Console.WindowWidth - 2; i++)
+            {
+                Console.Write('─');
+            }
+            Console.WriteLine('┐');
+            for (int i = 1; i <= Console.WindowHeight - 2; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write('│');
+            }
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.Write('└');
+            for (int i = 1; i <= Console.WindowWidth - 2; i++)
+            {
+                Console.Write('─');
+            }
+            Console.WriteLine('┘');
+            for (int i = 1; i <= Console.WindowHeight - 2; i++)
+            {
+                Console.SetCursorPosition(Console.WindowWidth - 1, i);
+                Console.Write('│');
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.ReadLine();
+        }
+    }
+}
